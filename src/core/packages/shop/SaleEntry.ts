b8e5ded59bf6ff8a66050dffb64ab4171ce6d3a9
doxylee <dylee063@gmail.com>
2021-10-28 17:34:34 +0900

    feat: Add shop package

---
+++ src/core/packages/shop/SaleEntry.ts
@@ -0,0 +1,15 @@
+import { DataObject } from "@core/common/dataobject";
+import BigNumber from "@core/common/BigNumber";
+
+export class SaleEntry extends DataObject {
+    itemId: string;
+    itemGroupId: string;
+    price: BigNumber;
+
+    constructor({ itemId, itemGroupId, price }: SaleEntry) {
+        super();
+        this.itemId = itemId;
+        this.itemGroupId = itemGroupId;
+        this.price = price;
+    }
+}
---
+++ src/core/packages/shop/ShopService.ts
@@ -0,0 +1,159 @@
+import { InvalidOperationException } from "@core/common/exceptions";
+import BigNumber from "@core/common/BigNumber";
+import { ShopRepository } from "@core/packages/shop/ShopRepository";
+import { CompanyService } from "@core/packages/company/CompanyService";
+import { TurnProgressSystem } from "@core/systems/TurnProgressSystem";
+import { ItemStorage } from "@core/packages/item/ItemStorage";
+import { ItemGroup } from "@core/packages/item/ItemGroup";
+import { Shop } from "@core/packages/shop/Shop";
+import {SaleEntry} from "@core/packages/shop/SaleEntry";
+
+const SHOP_PRICE_PER_SIZE = 200000;
+const SHOP_PRICE_PER_STORAGE_VOLUME = 20000;
+
+type SellingDataForTurnProgress = {
+    shopId: string;
+    itemGroup: ItemGroup;
+    price: BigNumber;
+};
+
+export class ShopService {
+    repository: ShopRepository;
+    userIdentity = {};
+
+    turnProgressSystem!: TurnProgressSystem;
+    companyService!: CompanyService;
+
+    constructor({ repository, userIdentity }: { repository: ShopRepository; userIdentity: {} }) {
+        this.repository = repository;
+        this.userIdentity = userIdentity;
+    }
+
+    initialize({ turnProgressSystem, companyService }: { turnProgressSystem: TurnProgressSystem; companyService: CompanyService }) {
+        this.turnProgressSystem = turnProgressSystem;
+        this.companyService = companyService;
+    }
+
+    /**
+     * Construct a new shop for the company.
+     * Company should have enough cash to construct.
+     *
+     * @param companyId
+     * @param size
+     * @param storageVolume
+     */
+    async createShop({ companyId, size, storageVolume }: { companyId: string; size: BigNumber; storageVolume: BigNumber }) {
+        // TODO: Don't construct shop, allow renting instead
+        const newShop = new Shop({ companyId, size, storage: new ItemStorage({ maxVolume: storageVolume }) });
+        const price = this.getShopConstructionPrice({ size, storageVolume });
+
+        // Try to withdraw money from company
+        await this.companyService.useCashOneTime({ id: companyId, amount: price });
+
+        // Construct Factory if successful
+        return await this.repository.create(newShop);
+
+        // TODO: If shop construction fails, withdrawn cash should be returned to the company.
+    }
+
+    /**
+     * Get price for constructing a new shop with given options.
+     *
+     * @param size
+     * @param storageVolume
+     */
+    getShopConstructionPrice({ size, storageVolume }: { size: BigNumber; storageVolume: BigNumber }) {
+        return size.times(SHOP_PRICE_PER_SIZE).plus(storageVolume.times(SHOP_PRICE_PER_STORAGE_VOLUME));
+    }
+
+    /**
+     * Get shop entity by id
+     * @param id
+     * @throws EntityNotFoundException
+     */
+    async getShop(id: string) {
+        return this.repository.read(id);
+    }
+
+    /**
+     * Get all shops of a company.
+     *
+     * @param companyId
+     */
+    async getShopsOfCompany({ companyId }: { companyId: string }) {
+        return this.repository.query({ filter: [["companyId", "=", companyId]], limit: null, showTotal: false });
+    }
+    
+    
+    /**
+     * Change which items the shop sells.
+     * WARNING: Signature might change
+     *
+     * @param shopId
+     * @param selling - Array of SaleEntries to sell from the shop
+     * @throws EntityNotFoundException
+     */
+    async setSelling({shopId, selling}: {shopId:string, selling: SaleEntry[]}){
+        return this.repository.update({id:shopId, selling});
+        // TODO: Don't allow replacing selling entirely
+        // TODO: Add validation
+    }
+
+    /**
+     * Get list of all SaleEntry data augmented with data needed for ConsumerMarket turn progression.
+     * Called from ConsumerMarketService.
+     *
+     * @throws EntityNotFoundException - Can't find ItemGroup designated in SaleEntry.
+     */
+    async getAllSellingDataForTurnProgress(): Promise<SellingDataForTurnProgress[]> {
+        const allShops = await this.repository.query({ limit: null, showTotal: false });
+        return allShops.flatMap((shop) =>
+            shop.selling.map((selling) => ({
+                shopId: shop.id,
+                itemGroup: shop.storage.getItemGroup(selling.itemGroupId),
+                price: selling.price,
+            }))
+        );
+        // TODO: itemGroupId in SaleEntry must meet FK restriction.
+    }
+
+    /**
+     * Shop buys contracted items.
+     *
+     * @param shopId
+     * @param itemGroup
+     * @param price
+     * @throws EntityNotFoundException
+     * @throws InvalidOperationException - Volume of items is bigger than storage
+     */
+    async buyItem({ shopId, itemGroup, price }: { shopId: string; itemGroup: ItemGroup; price: BigNumber }) {
+        const shop = await this.repository.read(shopId);
+        shop.storage.addItemGroup(itemGroup);
+        await this.repository.update(shop);
+        // TODO: Optimization opportunity
+
+        await this.companyService.useCashFixed({ id: shop.companyId, amount: price });
+    }
+
+    /**
+     * Shop sells goods.
+     *
+     * @param shopId
+     * @param itemGroupId
+     * @param amount
+     * @param price
+     * @throws EntityNotFoundException
+     * @throws InvalidOperationException - Insufficient amount of items in ItemGroup
+     */
+    async sellItem({ shopId, itemGroupId, amount, price }: { shopId: string; itemGroupId: string; amount: BigNumber; price: BigNumber }) {
+        const shop = await this.repository.read(shopId);
+
+        const items = shop.storage.takeItems({ itemGroupId, amount });
+        await this.repository.update(shop);
+        // TODO: Optimization opportunity
+
+        await this.companyService.gainRevenue({ id: shop.companyId, amount: price });
+
+        return items;
+    }
+}
