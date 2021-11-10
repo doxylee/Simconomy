64bed6068d6c7956d3baab766523f25c4223419e
doxylee <dylee063@gmail.com>
2021-11-11 02:33:30 +0900

    feat: Add utility types AwaitedReturnType, RecurseAwaitedReturnType

--- src/core/utils/UtilTypes.ts
+++ src/core/utils/UtilTypes.ts
@@ -1,4 +1,6 @@
 export type AwaitedType<T> = T extends PromiseLike<infer T> ? T : never;
 export type RecurseAwaitedType<T> = T extends PromiseLike<infer U> ? RecurseAwaitedType<U> : T;
+export type AwaitedReturnType<T extends (...args: any) => any> = AwaitedType<ReturnType<T>>;
+export type RecurseAwaitedReturnType<T extends (...args: any) => any> = RecurseAwaitedType<ReturnType<T>>;
 
 // Non negative integer: https://stackoverflow.com/a/69413070
