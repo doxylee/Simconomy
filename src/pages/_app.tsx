115b046ec9fa97dc4c156da41699ea9a8ff55a80
doxylee <dylee063@gmail.com>
2021-12-22 17:24:41 +0900

    fix: Disable Material button always being uppercase

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,10 +4,20 @@ import { CacheProvider } from "@emotion/react";
 import createCache from "@emotion/cache";
 
 import "tailwindcss/tailwind.css";
+import LayoutWrapper from "@src/components/common/LayoutWrapper";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 
 const cache = createCache({
     key: "css",
     prepend: true,
+});
+
+const theme = createTheme({
+    typography: {
+        button: {
+            textTransform: "none",
+        },
+    },
 });
 
 export default function SimconomyApp({ Component, pageProps }: AppProps) {
